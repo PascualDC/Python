6c424a08dfe64ce99c21feffd66ea938177e63b4
PascualDC <145747159+PascualDC@users.noreply.github.com>
2023-09-22 11:08:49 +0200

    Día 1

---
+++ Dia1.py
@@ -0,0 +1,50 @@
+#Que bonito es Python
+
+print ("hola mundo")
+print ("me llamo Pascual")
+
+#Nuestra primera variable
+
+x = 5
+
+print("print(5):")
+print(5)    #imprime 5
+print("print(5+5):")
+print(5+5)  #imprime 10
+print("print(x):")
+print(x)    #5
+print("print(x+10):")
+print(x+10) #15
+
+#creamos una variable con texto --> string
+
+nombre = "Pascual"
+numero = 8
+
+print(nombre)
+print("Sumo nombre+numero:")
+print('print("Celia"+"8")')
+print("Pascual"+"8")
+print('print(nombre+str(numero)):')
+print("Escribo 10 veces cucu")
+print("cucu"*10)
+
+#Hacemos operaciones matemáticas
+print()
+print("\nHacemos \"operaciones\"\\ matemáticas")
+
+num1 = 2
+num2 = 8
+
+suma = num1+num2
+resta = num2-num1
+multp = num1 * num2
+div = num1/num2
+
+print('suma = '+ str(suma))
+print('resta = ' + str(resta))
+print('suma = '+ str(suma)+' | resta = '+ str(resta))
+
+print('suma = '+ str(suma)+' | resta = '+ str(resta)+'\
+| multp = '+ str(multp)+ '| div = '+ str(div))
+
