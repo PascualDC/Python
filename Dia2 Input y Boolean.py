e51dd42c589514d470d0bb676694dfa008e0cd2c
PascualDC <145747159+PascualDC@users.noreply.github.com>
2023-09-25 14:22:44 +0200

    Add files via upload

---
+++ Dia2 Input y Boolean.py
@@ -0,0 +1,44 @@
+
+#El input recoge el dato y lo guarda en nombre.
+
+nombre = input("Introduce tu nombre: ")
+print("Hola " + nombre)
+
+
+#Existen dos tipos de = en la programación:
+#   =   --> Asigna lo que hay a la izquierda a lo que hay a la derecha.
+#           Ej. numero = 5
+#   ==   -> Es un igual lógico. ¡Compara valores que tiene a izq y der!
+#           Decuelve true, si son iguales; o false si son distintas.
+
+# 1. Entrada: Numero. Salida: True si es mayor de edad.
+#Input siempre guarda el valor introducuido con str.
+#Si queremos que sea un int hay que transformarlo a entero. int(input)...)
+
+#1 Edad
+#edad = int(input("Introduce tu edad: "))
+#Para saber el tipo --> print(type(edad))
+#print("¿Es mayor de edad? ")
+#print(edad >= 18)
+
+#2 Dias
+#dia = input("Introduce el día: ")
+#print("¿Es fin de semana? ")
+#print(dia == "Sabado" or dia == "Domingo")
+
+#3 Numero
+#numero = int(input("Introduce el número: "))
+#print("Es numero entero?")
+#print(numero >= 0)
+
+#4 Letra
+#letra = input("Introduce una letra: ")
+#print(letra == "a" or letra == "e" or letra == "i" or \
+#      letra == "o" or letra == "u")
+
+#5 Nota
+#nota = int(input("Introduce una nota: "))
+#print(nota >= 5)
+
+
+
